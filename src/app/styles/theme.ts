16ddc756788d744c678d99331c36dc4edf1b1973
Luiz Franco <luizfranco@wine.com.br>
2023-11-30 21:27:59 -0300

    feat/ ajustado eslint

--- src/app/styles/theme.ts
+++ src/app/styles/theme.ts
@@ -1,23 +1,23 @@
 const theme = {
-	colors: {
-		primary: "#F9F1E7",
-		secondary: "#B88E2F",
-		background: "##FFFFFF",
-		textColor: "#000000",
-		tomateColor: "#E97171",
-		chardonnayColor: "#2EC1AC",
-		grayPrimary: "#9F9F9F",
-		graySecondary: "##B0B0B0",
-		gray: "#E8E8E8",
-	},
-	fontSizes: {
-		small: "12px",
-		intermediate: "14px",
-		medium: "16px",
-		large: "18px",
-		semiLarge: "25px",
-		extraLarge: "52px",
-	},
+  colors: {
+    primary: "#F9F1E7",
+    secondary: "#B88E2F",
+    background: "#FFFFFF",
+    textColor: "#000000",
+    tomateColor: "#E97171",
+    chardonnayColor: "#2EC1AC",
+    grayPrimary: "#9F9F9F",
+    graySecondary: "#B0B0B0",
+    gray: "#E8E8E8",
+  },
+  fontSizes: {
+    small: "12px",
+    intermediate: "14px",
+    medium: "16px",
+    large: "18px",
+    semiLarge: "25px",
+    extraLarge: "52px",
+  },
 };
 
 export default theme;
